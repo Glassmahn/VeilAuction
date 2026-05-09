041ad7bcf6e1f017ed4594863d7275bf7b1f6457
Glassmahn <glassman4664@gmail.com>
2026-05-09 18:31:40 +0100

    Add experimental ESM externals configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  serverExternalPackages: ["@arcium-hq/client"],
+  experimental: {
+    esmExternals: "loose",
+  },
   turbopack: {},
 }
 
